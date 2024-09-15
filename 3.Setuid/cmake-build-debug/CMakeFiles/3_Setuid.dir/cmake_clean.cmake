222d6638cf6013126c34685711dd68baf79bdf08
MCBivis <Artemiybox@mail.ru>
2024-09-15 18:25:24 +0700

    complete 3 task

---
+++ 3.Setuid/cmake-build-debug/CMakeFiles/3_Setuid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3_Setuid"
+  "3_Setuid.pdb"
+  "CMakeFiles/3_Setuid.dir/main.c.o"
+  "CMakeFiles/3_Setuid.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3_Setuid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
