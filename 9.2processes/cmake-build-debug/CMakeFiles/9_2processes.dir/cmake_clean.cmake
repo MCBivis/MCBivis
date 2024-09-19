10de24d9dd67e621f945db8a94c582ec0fdae400
MCBivis <Artemiybox@mail.ru>
2024-09-19 14:52:04 +0700

    create 9_lab folder

---
+++ 9.2processes/cmake-build-debug/CMakeFiles/9_2processes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "9_2processes"
+  "9_2processes.pdb"
+  "CMakeFiles/9_2processes.dir/main.c.o"
+  "CMakeFiles/9_2processes.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/9_2processes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
