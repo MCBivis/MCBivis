ed900dfad335e06a8ca7c7d847f2aab9fb0cfe9e
MCBivis <Artemiybox@mail.ru>
2024-10-13 17:26:15 +0700

    21_lab

---
+++ 21.squeaker/cmake-build-debug/CMakeFiles/21_noicer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "21_noicer"
+  "21_noicer.pdb"
+  "CMakeFiles/21_noicer.dir/squeaker.c.o"
+  "CMakeFiles/21_noicer.dir/squeaker.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/21_noicer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
