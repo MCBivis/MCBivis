974878580942c8e8224035b179c9dcdf58bede78
MCBivis <Artemiybox@mail.ru>
2024-11-21 15:57:45 +0700

    28

---
+++ 28_rand/cmake-build-debug/CMakeFiles/28_rand.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "28_rand"
+  "28_rand.pdb"
+  "CMakeFiles/28_rand.dir/rand.c.o"
+  "CMakeFiles/28_rand.dir/rand.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/28_rand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
