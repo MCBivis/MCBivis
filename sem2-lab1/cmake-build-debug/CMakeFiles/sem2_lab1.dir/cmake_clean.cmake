31e11fad8c4a821962cb6114ca89b3411c8c9556
MCBivis <Artemiybox@mail.ru>
2025-02-04 13:34:00 +0700

    lab1

---
+++ sem2-lab1/cmake-build-debug/CMakeFiles/sem2_lab1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sem2_lab1.dir/main.c.o"
+  "CMakeFiles/sem2_lab1.dir/main.c.o.d"
+  "sem2_lab1"
+  "sem2_lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sem2_lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sem2-lab1/cmake-build-debug/CMakeFiles/sem2_lab1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sem2_lab1.
+# This may be replaced when dependencies are built.
